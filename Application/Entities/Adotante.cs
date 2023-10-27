8c84a44f27e20286ed0b5c86a0133b18eca9ccad
Nelson Barros <64411194+nelsonfogao@users.noreply.github.com>
2023-10-27 16:05:26 -0300

    Add files via upload

---
+++ Application/Entities/Adotante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Entities
+{
+    public abstract class Adotante : Account
+    {
+
+        public string CPF { get; set; }
+        public List<string> Fotos { get; set; }
+        public List<Guid> PetsFavoritos { get; set; }
+
+    }
+}
---
+++ Application/Entities/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Entities
+{
+    public class Pet
+    {
+        public Guid PetId { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public List<string> Fotos { get; set; }
+        public bool EhDog { get; set; }
+        public List<string> Caracteristicas { get; set; }
+        public List<Guid> AdotantesFavoritos { get; set; }
+
+    }
+}
